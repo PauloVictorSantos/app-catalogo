337c7bc4a0b42b86de4d4777957b3008cc7f081f
paulovictorln <paulovictor494@gmail.com>
2018-08-12 05:10:12 -0300

    listando itens com base na requisicao

--- src/components/ListaItens.js
+++ src/components/ListaItens.js
@@ -11,17 +11,30 @@ import {
   View
 } from 'react-native';
 
+import axios from 'axios';
 import Itens from './Itens';
 
 export default class ListaItens extends Component {
+
+        constructor(props){
+            super(props);
+            this.state = {  listaItens : [] };
+        }
+    componentWillMount() {
+        //requisicao https
+        //http://faus.com.br/recursos/c/dmairr/api/itens.html
+        axios.get('https://jsonplaceholder.typicode.com/photos').then(response=>{
+            this.setState({listaItens:  response.data});
+        }).catch(()=>{
+            console.log(error);
+        });
+    }
 
   render() {
       
     return (
       <View>
-          <Itens/>
-          <Itens/>
-          <Itens/>
+          {console.log(this.state.listaItens)}
       </View>
     );
   }
